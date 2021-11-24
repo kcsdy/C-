c221657c280f4fd4c7b52d60bda573d1130d984b
Kuntal Chakraborty <chakrabortykuntal27@gmail.com>
2021-11-24 14:31:21 +0530

    last commit

---
+++ Multiply.java
@@ -0,0 +1,8 @@
+public class Multiply {
+    public static void main(String[] args) {
+        int a=4;
+        int b=7;
+        System.out.println("the product s"+(a*b));
+    }
+    
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+    namespace loop
+    {
+        class program
+        {
+            static void Main(string [] args)
+            {
+                for(int i=0;i<5;i++)
+                {
+                    for(int j=0;j<=i;j++)
+                            Console.WriteLine("*");
+                }
+            }
+        }
+    }
