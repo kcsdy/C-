b95e044ddc6e0f9a5ebd062e715e4ef3a77113e3
Kuntal Chakraborty <chakrabortykuntal27@gmail.com>
2021-11-24 12:21:26 +0530

    added message in Sum.java

--- Sum.java
+++ Sum.java
@@ -3,6 +3,6 @@ class Sum
     public static void main(String[] args) {
         int a=-7;
         int b=9;
-        System.out.println(a+b);
+        System.out.println("The sum is:"+a+b);
     }
 }
